23b535427f645b5dc774222413a97ebb846cda3b
Alexei Yurikov <roboshadow@mail.ru>
2023-01-29 12:00:05 +0300

    add task2 solution

--- seminar1_homework.py
+++ seminar1_homework.py
@@ -18,3 +18,28 @@ else:
         print("The equilateral triangle")
     else:
         print("The isosceles triangle")
+
+
+# Напишите код, который запрашивает число и сообщает является ли оно простым или составным.
+# Используйте правило для проверки: “Число является простым, если делится нацело только на единицу и на себя”.
+# Сделайте ограничение на ввод отрицательных чисел и чисел больше 100 тысяч.
+
+LIMIT = 2
+
+while True:
+    num = int(input("Enter any number between 2 and 100 000: "))
+
+    if num < 0 or num > 100000:
+        print(f"{num} - wrong number!")
+        break
+
+    elif num == 0 or num == 1:
+        print(f"{num} is not the composite or prime number")
+
+    else:
+        for i in range(LIMIT, num):
+            if num % i == 0:
+                print(f"{num} is the composite number")
+                break
+        else:
+            print(f"{num} is the prime number")
