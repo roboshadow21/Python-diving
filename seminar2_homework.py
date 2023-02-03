c4d4fb93ecf6f47074d8955f48f552e615b780e4
Alexei Yurikov <roboshadow@mail.ru>
2023-02-03 20:47:41 +0300

    add fraction addition

--- seminar2_homework.py
+++ seminar2_homework.py
@@ -26,3 +26,32 @@ while num != 0:
 hex_num = reversed(result_list)
 
 print("".join(hex_num))
+
+# Напишите программу, которая принимает две строки вида
+# “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей.
+# Для проверки своего кода используйте модуль fractions.
+
+# fraction addition
+
+numerator1, denominator1 = map(int, (input("Enter fraction as a/b: ").split("/")))
+numerator2, denominator2 = map(int, (input("Enter fraction as c/d: ").split("/")))
+
+if denominator1 == 0 or denominator2 == 0:
+    print("Not a fraction!")
+
+else:
+    if denominator1 == denominator2:
+        print(f"{numerator1 + numerator2}/{denominator1}")
+
+    else:
+        numerator_result = (numerator1 * denominator2) + (numerator2 * denominator1)
+        denominator3 = denominator1 * denominator2
+        common_divider = gcd(numerator_result, denominator3)
+        print(f"{numerator_result // common_divider}/{denominator3 // common_divider}")
+
+# checking
+
+x = fraction.Fraction(numerator1, denominator1)
+y = fraction.Fraction(numerator2, denominator2)
+print(x + y)
