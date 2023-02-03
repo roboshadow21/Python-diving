d5c9b403cfa1ac2d25c4bb075a14f7257bfae071
Alexei Yurikov <roboshadow@mail.ru>
2023-02-03 20:45:07 +0300

    add task 1 solution

--- seminar2_homework.py
+++ seminar2_homework.py
@@ -1,2 +1,28 @@
 import fraction
 from math import gcd
+
+# Напишите программу, которая получает целое число и возвращает его
+# шестнадцатеричное строковое представление.
+# Функцию hex используйте для проверки своего результата.
+
+SAMPLE = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+BASE = 16
+
+result_list = []
+
+num = int(input("Enter decimal number: "))
+
+# checking
+
+print(str(hex(num))[2:])
+
+# convert to hex format
+
+while num != 0:
+    idx = num % BASE
+    result_list.append(SAMPLE[idx])
+    num //= BASE
+
+hex_num = reversed(result_list)
+
+print("".join(hex_num))
