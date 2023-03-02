8df6ed3244396e9b436683ad35481594766c21cc
Alexei Yurikov <roboshadow@mail.ru>
2023-03-02 15:53:57 +0300

    add convert_dict_to_csv file

---
+++ seminar8_homework/convert_dict_to_csv.py
@@ -0,0 +1,14 @@
+import csv
+
+
+def convert_dict_to_csv(data: dict) -> None:
+    rows = []
+    for k, v in data.items():
+        rows.append({k: v})
+
+    with open('result.csv', 'w', newline='', encoding='utf-8') as f:
+        fieldnames = ['directory', 'files', 'name', 'parent', 'size']
+
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
