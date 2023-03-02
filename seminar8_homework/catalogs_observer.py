798e24421e929de2ec8ad3889430d4fb04d89c62
Alexei Yurikov <roboshadow@mail.ru>
2023-03-02 16:13:22 +0300

    some fixes

--- seminar8_homework/catalogs_observer.py
+++ seminar8_homework/catalogs_observer.py
@@ -1,17 +1,14 @@
+from convert_dict_to_json import convert_to_json
+from convert_dict_to_csv import convert_to_csv
+from convert_dict_to_pickle import convert_to_pickle
+from pathlib import Path
+
 # Напишите функцию, которая получает на вход директорию и рекурсивно обходит
 # её и все вложенные директории. Результаты обхода сохраните в файлы json, csv и pickle.
 # * Для дочерних объектов указывайте родительскую директорию.
 # * Для каждого объекта укажите файл это или директория.
 # * Для файлов сохраните его размер в байтах, а для директорий размер файлов в ней с учётом
 # всех вложенных файлов и директорий.
-
-import json
-import csv
-import pickle
-from convert_dict_to_json import convert_to_json
-from convert_dict_to_csv import convert_to_csv
-from convert_dict_to_pickle import convert_to_pickle
-from pathlib import Path
 
 
 def catalogs_walk(catalog: Path) -> dict[str, list]:
